1f9fe7f0e66a2cbdf7885853d9d037db6d8e1d93
Daniel <danielspradah0@gmail.com>
2023-01-14 12:01:25 -0500

    Allow element creation via index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+
+const achu = document.createElement("div");
+
+console.log("WERBP");
+achu.innerHTML = "Added via webpack";
+achu.classList.add('achu');
+document.body.appendChild(achu);
